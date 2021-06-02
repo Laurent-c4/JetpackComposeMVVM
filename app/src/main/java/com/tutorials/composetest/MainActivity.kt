238781a333157e5f07a52cdb08f66628339c5b95
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-06-02 13:06:36 +0100

    Initial Commit

---
+++ app/src/main/java/com/tutorials/composetest/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.tutorials.composetest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.tutorials.composetest.ui.theme.ComposeTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(text = "Only way is up")
+                Spacer(modifier = Modifier.padding(top = 10.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Wolandeh")
+                }
+            }
+        }
+    }
+}
