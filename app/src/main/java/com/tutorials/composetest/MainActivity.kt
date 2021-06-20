ba81759c1b92a9efae7bdbaa11cfad47fdaf980b
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-06-20 13:14:17 +0100

    Fragments and Interoperability

--- app/src/main/java/com/tutorials/composetest/MainActivity.kt
+++ app/src/main/java/com/tutorials/composetest/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tutorials.composetest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
@@ -24,71 +25,12 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.tutorials.composetest.ui.theme.ComposeTestTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-//            Column {
-//                Column(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(200.dp)
-//                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-//                    verticalArrangement = Arrangement.Center
-//                ) {
-//                    Text(text = "Item 1", modifier = Modifier.align(Alignment.CenterHorizontally))
-//                    Text(text = "Item 2", modifier = Modifier.align(Alignment.CenterHorizontally))
-//                }
-//
-//                Spacer(modifier = Modifier.padding(20.dp))
-//
-//                Row(
-//                    modifier = Modifier
-//                        .width(200.dp)
-//                        .height(200.dp)
-//                        .border(border = BorderStroke(width = 1.dp, color = Color.Black)),
-//                    horizontalArrangement = Arrangement.Center
-//                ) {
-//                    Text(text = "Item 2", modifier = Modifier.align(Alignment.CenterVertically))
-//                }
-//            }
+        setContentView(R.layout.activity_main)
 
-
-            Column(
-                modifier = Modifier
-                    .verticalScroll(rememberScrollState())
-                    .background(color = Color(0xFFF2F2F2))
-                    .fillMaxSize()
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.happy_meal_small),
-                    contentDescription = "content desc",
-                    modifier = Modifier.height(300.dp),
-                    contentScale = ContentScale.Crop,
-                )
-                Column(modifier = Modifier.padding(16.dp)) {
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceBetween
-                    ) {
-                        Text(
-                            text = "Dimbua Kuku",
-                            style = TextStyle(fontSize = 26.sp)
-                        )
-                        Text(
-                            text = "$5.99",
-                            style = TextStyle(color = Green, fontSize = 17.sp),
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Text(text = "500 Cal", style = TextStyle(fontSize = 17.sp))
-                    Spacer(modifier = Modifier.padding(top = 10.dp))
-                    Button(onClick = { /*TODO*/ },modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
-                        Text(text = "Order Now")
-                    }
-                }
-            }
-        }
+        supportFragmentManager.beginTransaction().replace(R.id.main_container, RecipeListFragment())
+            .commit()
     }
 }
