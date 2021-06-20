b9709133c2067f4f613a735c26d39f8a7390051b
Laurent-c4 <laurentjuma.c4@gmail.com>
2021-06-20 14:47:29 +0100

    Navigation Component

--- app/src/main/java/com/tutorials/composetest/RecipeFragment.kt
+++ app/src/main/java/com/tutorials/composetest/RecipeFragment.kt
@@ -4,26 +4,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 
-class RecipeListFragment : Fragment() {
+class RecipeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -32,11 +23,7 @@ class RecipeListFragment : Fragment() {
         return ComposeView(requireContext()).apply {
             setContent {
                 Column(modifier = Modifier.padding(16.dp)) {
-                    Text(text = "Recipe List", style = TextStyle(fontSize = 21.sp))
-                    Spacer(modifier = Modifier.padding(10.dp))
-                    Button(onClick = { findNavController().navigate(R.id.viewRecipe) }) {
-                        Text(text = "GO TO RECIPE FRAGMENT")
-                    }
+                    Text(text = "RECIPE", style = TextStyle(fontSize = 21.sp))
                 }
             }
         }
